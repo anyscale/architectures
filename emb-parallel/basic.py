17239a9a03105c5e8acd245b3ed40e180924f491
Charles Greer <cgreer@anyscale.com>
2021-11-03 09:28:32 -0700

    uncomment

--- emb-parallel/basic.py
+++ emb-parallel/basic.py
@@ -1,15 +1,17 @@
-# import things
+## import things
+import ray
 import pandas as pd
 from prophet import Prophet
 
-# data pre-processing
+## data pre-processing
 df = pd.read_csv('./yellow_tripdata_2021-01.csv')
 df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"] ).dt.date.astype("datetime64")
 df= df[["tpep_pickup_datetime", "VendorID", "PULocationID"]]
-df = df.rename(columns={"tpep_pickup_datetime":"ds", "VendorID":"y"}).groupby([df["ds"], df["PULocationID"]]).count().reset_index()
+df = df.rename(columns={"tpep_pickup_datetime":"ds", "VendorID":"y"})
+df = df.groupby([df["ds"], df["PULocationID"]]).count().reset_index()
 loc_list = df["PULocationID"].unique()
 
-# vanilla impl without ray
+## vanilla impl without ray
 result = {}
 for i in loc_list:
     m = Prophet()
@@ -18,13 +20,13 @@ for i in loc_list:
 
 
 
-# ray connection
-ray.init()
+## ray connection
+ray.init("anyscale://ci_cd_architecture", log_to_driver=False, runtime_env={"pip":["prophet"],"excludes":["yellow*"]})
 @ray.remote
 def fit_prophet(i):
     m = Prophet()
     m.fit(df[df["PULocationID"]==i])
-    result[i]=m 
+    return m
 
 ## Fire Hose Approach -- fire as fast as you can and wait for the result
 result = []
@@ -37,7 +39,7 @@ ray.get(result)
 result = []
 max_tasks = 10 # specifying the max number of results
 for i in loc_list:
-	if len(result) > max_tasks:
+    if len(result) > max_tasks:
         # calculating how many results should be available
         num_ready = len(result)-max_tasks
         # wait for num_returns to be equal to num_ready, ensuring the amount of task in flight is checked
